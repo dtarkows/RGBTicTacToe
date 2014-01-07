cbe2b84c65e88d419fd53ea9fbf0cfa1b8b0b4ee
dtarkows <dtarkows@gmail.com>
2014-01-06 22:33:09 -0500

    Draw some X's
    Draw X's across the first row of the board.

--- RGBTicTacToe/RGBTicTacToe.ino
+++ RGBTicTacToe/RGBTicTacToe.ino
@@ -27,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-  
+
   // Draw a red line for the first horizontal line
   matrix.drawLine(0, 10, 31, 10, matrix.Color333(7, 0, 0));
   // Draw a green line for the second horizontal line
@@ -36,6 +36,25 @@ void loop() {
   matrix.drawLine(10, 0, 10, 31, matrix.Color333(0, 0, 7));
   // Draw a purple line for the second vertical line
   matrix.drawLine(21, 0, 21, 31, matrix.Color333(7, 0, 7));
+
+  drawX();
+
+}
+
+void drawX(void) {
+  uint8_t startX = 0;
+  uint8_t startY = 0;
+  uint8_t boxWidth = 9;
   
+  matrix.drawLine(startX, startY, startX+boxWidth, startY+boxWidth, matrix.Color333(7,7,7));
+  matrix.drawLine(startX, startY+boxWidth, startX+boxWidth, startY, matrix.Color333(7,7,7));
+  
+  startX = startX + boxWidth+2;
+  matrix.drawLine(startX, startY, startX+boxWidth, startY+boxWidth, matrix.Color333(7,7,7));
+  matrix.drawLine(startX, startY+boxWidth, startX+boxWidth, startY, matrix.Color333(7,7,7));
+  
+  startX = startX + boxWidth+2;
+  matrix.drawLine(startX, startY, startX+boxWidth, startY+boxWidth, matrix.Color333(7,7,7));
+  matrix.drawLine(startX, startY+boxWidth, startX+boxWidth, startY, matrix.Color333(7,7,7));
 }
 
